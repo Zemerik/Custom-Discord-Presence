2f880264b0d20082e319520d51a04a16e7426df7
Hemang Yadav <zemerikY@gmail.com>
2023-12-11 17:50:25 +1100

    Presence Details

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // For assistance, look at presence.png file
+    showTime: , // Toggle to show or hide time in status (true/false)
+    token: "", // Acount token here
+    timeZone: "", // Your Timezone, eg Asia/Kolkata
+    Name: "", // The name of your presence
+    State: "", // The state of your presence
+    Details: "", // Details of your presence
+    LargeImage: "", // DISCORD CDN IMAGE ONLY
+    LargeText: "", // Hover text for large image
+    SmallImage: "", // DISCORD CDN IMAGE ONLY
+    SmallText: "", // Hover text for small image
+    FirstButtonName: "", // Button name
+    FirstButtonUrl: "", // Button URL
+    SecondButtonName: "", // Button name
+    SecondButtonUrl: "", // Button URL
+  };
+  
